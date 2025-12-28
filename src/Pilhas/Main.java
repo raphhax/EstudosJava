d2eea06afd5cff04958abe57b4f3ef207c1db7d1
raphhax <raphaelhenriquedev@gmail.com>
2025-12-27 21:19:54 -0300

    refatorando estruturas de dados

--- src/Pilhas/Main.java
+++ src/Pilhas/Main.java
@@ -1,23 +1,37 @@
 package Pilhas;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args){
         // static size, some 2 slots of memory
+        arraysBasicos();
+
+        // List names, dynamic sizes(automatic up or down)
+        lista();
+
+        // Stack names
+        pilhas();
+
+        // QUEUE(FILAS)
+        filas();
+    }
+
+    public static void arraysBasicos(){
         String[] namesArray = new String[2];
         namesArray[0] = "Narutim";
         namesArray[1] = "Narutao";
+    }
 
-        // List names, dynamic sizes(automatic up or down)
+    public static void lista(){
         List<String> namesList = new ArrayList<>();
         namesList.add("Narutadas");
         System.out.println(namesList);
+    }
 
-        // Stack names
+    public static void pilhas(){
         Stack<String> namesStack = new Stack<>();
         namesStack.push("Naruto");
         namesStack.push("Charutadas");
@@ -27,5 +41,10 @@ public class Main {
         namesStack.peek(); // View the actually top element of my stack
         System.out.println("Finished Stack: " + namesStack);
         System.out.println("My final stack size: " + namesStack.size());
+    }
+
+    public static void filas(){
+        Queue<String> namesQueue = new LinkedList<>();
+        namesQueue.add("");
     }
 }
